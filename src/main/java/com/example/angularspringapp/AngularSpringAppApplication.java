d2276c0765a3dedb52bf0c4f6d5e414001e2d5e8
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-17 23:03:07 +0100

    initial commit

---
+++ src/main/java/com/example/angularspringapp/AngularSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/angularspringapp/AngularSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
